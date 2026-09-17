95c843501bd247849ed5b24acc89f4fd244cd5c8
Mohammad <mohm.salamon@gmail.com>
2026-09-17 12:23:07 +0330

    اصلاح کامل فایل server.js و تقویم آبشاری

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const fs = require("fs");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // ENV
 const TOKEN = process.env.TOKEN;
